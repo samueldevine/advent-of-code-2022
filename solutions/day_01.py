c7f72209b4411805d3a6e6968269ba5b9dd4b2f9
Sam Devine <samuel.will.devine@gmail.com>
2022-12-02 12:02:40 -0700

    refactor day 1

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -1,24 +1,6 @@
 with open('inputs/day_01.txt') as input_file:
     lines = input_file.readlines()
 
-# part 1
-max_calories = 0
-total_calories = 0
-
-for line in lines:
-    cals = line.strip()
-    if cals == "":
-        if total_calories > max_calories:
-            max_calories = total_calories
-        total_calories = 0
-        continue
-    else:
-        total_calories += int(cals)
-
-print(max_calories)
-
-# part 2
-total_calories_top_3 = 0
 individual_calorie_counter = 0
 calorie_totals = []
 
@@ -32,4 +14,9 @@ for line in lines:
         individual_calorie_counter += int(cals)
 
 calorie_totals.sort(reverse=True)
+
+# part 1 answer
+print(calorie_totals[0])
+
+# part 2 answer
 print(sum(calorie_totals[0:3]))
