4628bcdb61816961f97827930b7fd9b39ed9280e
Sam Devine <samuel.will.devine@gmail.com>
2022-12-02 15:49:52 -0700

    part 2

--- solutions/day_02.py
+++ solutions/day_02.py
@@ -7,29 +7,48 @@ total_score = 0
 for round in rounds:
     opponent = round[0]
     my_play = round[2]
-    if my_play == "X":
+    if my_play == "X": # rock
         total_score += scores["X"]
-        if opponent == "A":
+        if opponent == "A": # rock
             total_score += scores["draw"]
-        if opponent == "B":
+        if opponent == "B": # paper
             total_score += scores["loss"]
-        if opponent == "C":
+        if opponent == "C": # scissors
             total_score += scores["win"]
-    if my_play == "Y":
+    if my_play == "Y": # paper
         total_score += scores["Y"]
-        if opponent == "A":
+        if opponent == "A": # rock
             total_score += scores["win"]
-        if opponent == "B":
+        if opponent == "B": # paper
             total_score += scores["draw"]
-        if opponent == "C":
+        if opponent == "C": # scissors
             total_score += scores["loss"]
-    if my_play == "Z":
+    if my_play == "Z": # scissors
         total_score += scores["Z"]
-        if opponent == "A":
+        if opponent == "A": # rock
             total_score += scores["loss"]
-        if opponent == "B":
+        if opponent == "B": # paper
             total_score += scores["win"]
-        if opponent == "C":
+        if opponent == "C": # scissors
             total_score += scores["draw"]
 
-print(total_score)
+print(f"Part 1 total score: {total_score}")
+
+# part 2
+loss_map = {"A": 3, "B": 1, "C": 2}
+draw_map = {"A": 4, "B": 5, "C": 6}
+win_map = {"A": 8, "B": 9, "C": 7}
+
+total_score = 0
+for round in rounds:
+    opponent = round[0]
+    result = round[2]
+
+    if result == "X": # loss
+        total_score += loss_map[opponent]
+    if result == "Y": # draw
+        total_score += draw_map[opponent]
+    if result == "Z": # win
+        total_score += win_map[opponent]
+
+print(f"Part 2 total score: {total_score}")
