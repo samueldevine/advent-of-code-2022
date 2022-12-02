bda0e52670ce76c9b32e9aa759df19c5bcd8b1dc
Sam Devine <samuel.will.devine@gmail.com>
2022-12-02 12:01:02 -0700

    day 1 part 2

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -1,6 +1,7 @@
 with open('inputs/day_01.txt') as input_file:
     lines = input_file.readlines()
 
+# part 1
 max_calories = 0
 total_calories = 0
 
@@ -15,3 +16,20 @@ for line in lines:
         total_calories += int(cals)
 
 print(max_calories)
+
+# part 2
+total_calories_top_3 = 0
+individual_calorie_counter = 0
+calorie_totals = []
+
+for line in lines:
+    cals = line.strip()
+    if cals == "":
+        calorie_totals.append(individual_calorie_counter)
+        individual_calorie_counter = 0
+        continue
+    else:
+        individual_calorie_counter += int(cals)
+
+calorie_totals.sort(reverse=True)
+print(sum(calorie_totals[0:3]))
