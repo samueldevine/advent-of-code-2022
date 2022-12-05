b7131dada728598f29669596b2bb8d419032f577
Sam Devine <samuel.will.devine@gmail.com>
2022-12-05 10:12:17 -0700

    part 2

--- solutions/day_03.py
+++ solutions/day_03.py
@@ -7,6 +7,9 @@ priorities = [
     "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
     "T", "U", "V", "W", "X", "Y", "Z",
 ]
+
+# part 1
+
 sum_of_priorities = 0
 for sack in sacks:
     sack = sack.strip()
@@ -18,5 +21,26 @@ for sack in sacks:
     priority = priorities.index(repeated_item) + 1
 
     sum_of_priorities += priority
+
+print(sum_of_priorities)
+
+# part 2
+
+sum_of_priorities = 0
+elf_1 = 0
+
+for sack in sacks:
+    if elf_1 >= len(sacks) - 1:
+        break
+
+    sack_1 = sacks[elf_1].strip()
+    sack_2 = sacks[elf_1 + 1].strip()
+    sack_3 = sacks[elf_1 + 2].strip()
+
+    badge = ''.join(set(sack_1) & set(sack_2) & set(sack_3))
+    priority = priorities.index(badge) + 1
+    sum_of_priorities += priority
+
+    elf_1 += 3
 
 print(sum_of_priorities)
