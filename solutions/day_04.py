a2f131107c134810f8bfccefac3bc8ce9dbfa9f3
Sam Devine <samuel.will.devine@gmail.com>
2022-12-05 15:36:04 -0700

    part 2

--- solutions/day_04.py
+++ solutions/day_04.py
@@ -14,10 +14,31 @@ for assignment in cleaning_assignments:
     start_2 = int(elf_2[0])
     finish_2 = int(elf_2[1])
 
-
     if start_1 <= start_2 and finish_1 >= finish_2:
         pairs_for_reconsideration += 1
     elif start_2 <= start_1 and finish_2 >= finish_1:
+        pairs_for_reconsideration += 1
+
+print(pairs_for_reconsideration)
+
+# part 2
+pairs_for_reconsideration = 0
+
+for assignment in cleaning_assignments:
+    pair = assignment.strip().split(',')
+    elf_1 = pair[0].split('-')
+    elf_2 = pair[1].split('-')
+
+    start_1 = int(elf_1[0])
+    finish_1 = int(elf_1[1])
+    start_2 = int(elf_2[0])
+    finish_2 = int(elf_2[1])
+
+    if start_1 == start_2 or finish_1 == finish_2:
+        pairs_for_reconsideration += 1
+    elif start_1 < start_2 and finish_1 >= start_2:
+        pairs_for_reconsideration += 1
+    elif start_2 < start_1 and finish_2 >= start_1:
         pairs_for_reconsideration += 1
 
 print(pairs_for_reconsideration)
