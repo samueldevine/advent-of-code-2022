91e2a3e2f8320b99866becb248a7272ea2f5db64
Sam Devine <samuel.will.devine@gmail.com>
2022-12-07 15:20:29 -0700

    part 2

--- solutions/day_06.py
+++ solutions/day_06.py
@@ -3,6 +3,7 @@ with open("inputs/day_06.txt") as input_file:
 
 chars = list(stream)
 
+# part 1
 counter = 0
 last_4 = ["r", "r", "r", "r"]
 
@@ -15,4 +16,19 @@ for char in chars:
     counter += 1
 
 print(last_4)
+print(counter)
+
+# part 2
+counter = 0
+last_14 = ["r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r"]
+
+for char in chars:
+    if len(set(last_14)) == len(last_14):
+        break
+
+    last_14.append(char)
+    del last_14[0]
+    counter += 1
+
+print(last_14)
 print(counter)
